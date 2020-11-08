7f09db673b95f8b12cb13cc351b72018cf1dfd1b
Richard Huang <ziluoh@gmail.com>
2020-11-07 16:59:50 -0800

    Finish

--- buttons&popups/main.js
+++ buttons&popups/main.js
@@ -1,1 +1,37 @@
-console.log("ojk");
+const cancelBtn = document.getElementById('button-cancel');
+const acceptBtn = document.getElementById('button-accept');
+const popupThank = document.getElementById('popup-top');
+const popupWelcome = document.getElementById('popup-bottom');
+const container = document.getElementById('container');
+
+cancelBtn.addEventListener('click', function () {
+	popupWelcome.classList.add('show');
+	timer(popupWelcome);
+});
+
+acceptBtn.addEventListener('click', function () {
+	popupThank.classList.add('show');
+	timer(popupThank);
+});
+
+container.addEventListener('click', function(e) {
+	let excludeTargets = [
+		'button-cancel',
+		'button-accept',
+		'popup-top',
+		'popup-bottom',
+	];
+
+    // if target is not target button nor popups, remove it
+	if (!excludeTargets.includes(e.target.id)) {
+		popupThank.classList.remove('show');
+        popupWelcome.classList.remove('show');
+	}
+});
+
+// popup remove show class after 8s 
+const timer = (element) => {
+	setTimeout(() => {
+		element.classList.remove('show');
+	}, 8000);
+};
